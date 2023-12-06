165a974fb02a6156d1971633382898a614e456f2
Francieli Alvarenga <francieli.franco@grupoboticario.com.br>
2023-12-06 14:04:10 -0300

    create home page, and gitignore

---
+++ scripts/main.js
@@ -0,0 +1,74 @@
+// botao menu
+const btnMenu = document.getElementById("botao-menu");
+const menu = document.getElementById("menu-mobile");
+const overlay = document.getElementById("overlay-menu");
+
+btnMenu.addEventListener("click", () => {
+  menu.classList.add("abrir-menu");
+});
+
+menu.addEventListener("click", () => {
+  menu.classList.remove("abrir-menu");
+});
+
+overlay.addEventListener("click", () => {
+  menu.classList.remove("abrir-menu");
+});
+
+// tema claro/escuro
+
+const chk = document.getElementById("chk");
+
+function toggleCleanmode(){
+  document.body.classList.toggle("clean");
+}
+
+function loadTheme(){
+  const cleanMode = localStorage.getItem('clean')
+  if(cleanMode){
+    toggleCleanmode();
+  }
+}
+
+loadTheme();
+
+chk.addEventListener("change", () => {
+  toggleCleanmode();
+
+
+  localStorage.removeItem('clean')
+
+  if(document.body.classList.contains('clean')){
+    localStorage.setItem('clean',1)
+  }
+});
+
+// Envio de formulario 
+
+const formE1 = document.getElementById('form-api');
+
+formE1.addEventListener('submit', evento => {
+  evento.preventDefault();
+  
+  const formDados = new FormData(formE1);
+  const data = Object.fromEntries(formDados.entries());
+
+  fetch('https://formspree.io/f/mpzggbdb', {
+    method: 'POST',
+    body: formDados,
+    headers: {
+      'Accept': 'application/json'
+    }
+  }).then(response => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error('Erro ao enviar o formulário');
+    }
+  }).then(data => {
+    console.log('Formulário enviado com sucesso:', data);
+    window.location.href = '../pages/obrigado.html';
+  }).catch(error => {
+    console.error('Erro:', error);
+  });
+});
